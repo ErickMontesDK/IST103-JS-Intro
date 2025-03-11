e509b346bd32b05ae06b457e894fe437d56d2b97
ErickDKnight <erickmontesdk@gmail.com>
2025-03-10 20:13:35 -0700

    created person variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello World")
+// alert("Hello World")
+
+const first_name = "Abril";
+const last_name = "Montes Bedolla";
+const age = 23;
+const occupation = "student";
+const single = true;
+let dependents = null;
+let job;
+
