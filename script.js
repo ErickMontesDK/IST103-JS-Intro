1b7a9afa6bc73a9e3ba400ec93db85389ed0d47a
ErickDKnight <erickmontesdk@gmail.com>
2025-03-10 20:19:58 -0700

    Erick - Class Activity 2 - 10 March 2025 #2

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const single = true;
 let dependents = null;
 let job;
 
+const full_name = first_name + " " + last_name;
+console.log("Person's name: " + full_name);
+console.log("Age: " + age);
+console.log("The person is single: " + single);
+console.log("Person's occupation: " +occupation);
+console.log("List of dependents: " + dependents);
+console.log("Work industry: " + job);
